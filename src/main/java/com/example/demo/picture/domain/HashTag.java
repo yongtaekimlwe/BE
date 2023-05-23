eb1383eecd090dc13ec239338a1ffb82e36891ae
김태용 <amico741@naver.com>
2023-05-23 16:57:18 +0900

    feat: 사진 공유 관련 엔티티 추가

---
+++ src/main/java/com/example/demo/picture/domain/HashTag.java
@@ -0,0 +1,34 @@
+package com.example.demo.picture.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Entity
+@Table(name = "hashtag")
+public class HashTag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "tag_id")
+    private int tagId;
+
+    @Column(name="tag_name")
+    private String tagName;
+
+    @Column(name="tag_icon")
+    private String tagIcon;
+
+    @ManyToMany(mappedBy = "hashtags")
+    private List<PictureBoard> imageBoards;
+
+    public HashTag() { }
+
+    @Builder
+    public HashTag(String tagName, String tagIcon) {
+        this.tagName = tagName;
+        this.tagIcon = tagIcon;
+    }
+}
---
+++ src/main/java/com/example/demo/picture/domain/PictureBoard.java
@@ -0,0 +1,53 @@
+package com.example.demo.picture.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.List;
+
+@Getter
+@Entity
+@Table(name="image_board")
+@EntityListeners(AuditingEntityListener.class)
+public class PictureBoard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private int imageId;
+
+//    @ManyToOne
+//    @JoinColumn(name= "user_id")
+//    private User user;
+
+//    @ManyToMany
+//    @JoinTable(name = "user_image_like",
+//            joinColumns = @JoinColumn(name = "image_id"),
+//            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    //private Set<User> likedByUsers = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(name = "image_board_hashtag",
+            joinColumns = @JoinColumn(name = "image_board_image_id"),
+            inverseJoinColumns = @JoinColumn(name = "hashtag_tag_id"))
+    private List<HashTag> hashtags;
+
+
+    private String title;
+
+    private String content;
+
+    @Column(name="image_url")
+    private String imageUrl;
+
+    public PictureBoard() {
+    }
+    @Builder
+    public PictureBoard(String title, String content, String imageUrl) {
+        this.title = title;
+        this.content = content;
+        this.imageUrl = imageUrl;
+    }
+}
